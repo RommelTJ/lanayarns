3255a2f086da6c51e3aed37e944a429ae3514bea
Rommel Rico <rommeltj@gmail.com>
2016-08-14 08:28:44 -0700

    Handling form submission

--- src/comingsoon/urls.py
+++ src/comingsoon/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
+from django.shortcuts import redirect
+from django.views.generic import TemplateView
 
 from . import views
 
 urlpatterns = [
     # ex: /comingsoon/
     url(r'^$', views.InterestedUserCreate.as_view(), name='index'),
+    # ex: /comingsoon/thanks/
+    url(r'^thanks/$', TemplateView.as_view(template_name="comingsoon/thanks.html"), name='thanks'),
 ]
